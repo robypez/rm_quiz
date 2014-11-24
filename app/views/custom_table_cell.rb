6769831f151bcfa4b675c383dcaa8540bcc3dcd0
Jamon Holmgren <jamon@clearsightstudio.com>
2014-11-23 19:51:15 -0800

    Implement table selection

--- app/views/custom_table_cell.rb
+++ app/views/custom_table_cell.rb
@@ -1,12 +1,12 @@
 class CustomTableCell < UITableViewCell
- 
+
   # This method is used by ProMotion to instantiate cells.
   def initWithStyle(style_name, reuseIdentifier: reuseIdentifier)
     super
-     stylish
+    stylish
     self
   end
-  
+
   # A delegate method when the user clicks the Row(it's blue by default)
   def setHighlighted(highlighted, animated: animated)
     if highlighted
@@ -15,9 +15,15 @@ class CustomTableCell < UITableViewCell
       self.backgroundColor = UIColor.whiteColor
     end
   end
-  
- def stylish
+
+  def stylish
     self.backgroundColor = UIColor.whiteColor
     self.selectionStyle = UITableViewCellSelectionStyleNone
   end
+
+  def selected=(is_selected)
+    bg_color = is_selected ? UIColor.redColor : UIColor.whiteColor
+    self.backgroundColor = bg_color
+  end
+
 end
