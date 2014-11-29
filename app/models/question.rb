a01856278c0ca56a71f60da02f942cfbe6e78c93
Roberto Pezzali <scrivoaroby@gmail.com>
2014-11-29 10:04:15 +0100

    push json

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,15 +3,15 @@ class Question < CDQManagedObject
   @question = ""
 
   def self.load_json
-    seed_file = NSBundle.mainBundle.pathForResource('question2', ofType:'json')
+    seed_file = NSBundle.mainBundle.pathForResource('question', ofType:'json')
     contents = NSData.dataWithContentsOfFile(seed_file)
-    contents_string = NSString.alloc.initWithData(contents, encoding:NSUTF8StringEncoding)
-    # json_string = String.new(NSString.stringWithContentsOfFile(seed_file))
-    # BW::JSON.parse(json_string)
+    # contents_string = NSString.alloc.initWithData(contents, encoding:NSUTF8StringEncoding)
+    json_string = String.new(NSString.stringWithContentsOfFile(seed_file))
+    BW::JSON.parse(json_string)
   end
 
   def self.load_url
-    AFMotion::JSON.get("http://media.dday.it/roby/question2.json") do |result|  
+    AFMotion::JSON.get("http://media.dday.it/roby/question.json") do |result|  
       if result.success?
         stuff_received result.body
       elsif result.failure?
